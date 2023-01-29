336df1a3f5005a03ea5d6be70062da5712f47920
LAI HOP HIEP <39963983+laihophiep@users.noreply.github.com>
2023-01-29 16:26:02 +0700

    Update Adding custom admin image upload metabox field in woocommerce to product category.php

--- Adding custom admin image upload metabox field in woocommerce to product category.php
+++ Adding custom admin image upload metabox field in woocommerce to product category.php
@@ -1,3 +1,103 @@
+// add to function.php
+//call for woocommerce custom admin image code    
+require get_template_directory() . '/inc/woo-meta-category.php';
+
+/*--------------------------------------------------------------------------------------
+    Uploader JS
+----------------------------------------------------------------------------------------*/
+function my_admin_scripts() {
+    wp_enqueue_media();
+
+    wp_register_script( 'wina_classic-uadmin-js', get_template_directory_uri() . '/js/uploader.js', 
+                                        array('jquery','media-upload','thickbox'), '20130115', true );
+    wp_enqueue_script( 'wina_classic-uadmin-js');
+}
+
+add_action('admin_print_scripts', 'my_admin_scripts');
+
+/*--------------------------------------------------------------------------------------
+
+//Add Media uploader Javascript in js/uploader.js
+jQuery(document).ready( function($){
+
+    var mediaUploader_woo;
+
+    $('#upload-button-woo').on('click',function(e) {
+        e.preventDefault();
+        if( mediaUploader_woo ){
+            mediaUploader_woo.open();
+            return;
+        }
+
+        mediaUploader_woo = wp.media.frames.file_frame = wp.media({
+            title: 'Choose an Image',
+            button: { text: 'Choose Image'},
+            multiple: false
+        });
+
+        mediaUploader_woo.on('select', function(){
+            attachment = mediaUploader_woo.state().get('selection').first().toJSON();
+            $('#category-meta-woo').val(attachment.url);
+            $('#category-header-preview').attr('src', ''+ attachment.url + '' );
+        });
+
+        mediaUploader_woo.open();
+    });    
+
+});
+<?php
+
+/*-------------------------------------------------------------------
+    Add Custom metabox for woocommerce Category page
+---------------------------------------------------------------------*/
+
+function product_cat_add_cat_head_field_rj() {  ?>
+    <div class="form-field">
+        <label for="term_meta[cat_head_link]"><?php _e( 'Category Page Image', 'wina-classic' ); ?></label>
+        <input type="text" name="term_meta[cat_head_link]" id="term_meta[cat_head_link]" value="">
+        <p class="description"><?php _e( 'Upload Category Page Image','wina-classic' ); ?></p>
+    </div>
+<?php }
+
+function product_cat_edit_cat_head_field_rj($term) {
+    $t_id = $term->term_id; $term_meta = get_option( "taxonomy_$t_id" ); ?>
+
+    <tr class="form-field">
+    <th scope="row" valign="top"><label for="term_meta[cat_head_link]"><?php _e( 'Category Page Image', 'wina-classic' ); ?></label></th>
+        <td>
+            <img src="<?php echo esc_attr( $term_meta['cat_head_link'] ) ? esc_attr( $term_meta['cat_head_link'] ) : ''; ?>" height="60" width="120" id="category-header-preview" />
+            <input type="hidden" name="term_meta[cat_head_link]" id="category-meta-woo" value="<?php echo esc_attr( $term_meta['cat_head_link'] ) ? esc_attr( $term_meta['cat_head_link'] ) : ''; ?>" style="margin-left: 0px; margin-right: 0px; width: 50%;" />
+            <input type="button" class="button button-secondary" value="Upload Image" id="upload-button-woo" />
+            <p class="description"><?php _e( 'Upload Category Page Image','wina-classic' ); ?></p>
+        </td>
+    </tr>
+<?php
+}
+
+// this action use for add field in add form of taxonomy 
+add_action( 'product_cat_add_form_fields', 'product_cat_add_cat_head_field_rj', 10, 2 );
+// this action use for add field in edit form of taxonomy 
+add_action( 'product_cat_edit_form_fields', 'product_cat_edit_cat_head_field_rj', 10, 2 );
+
+function product_cat_cat_head_link_save( $term_id ) {
+    if ( isset( $_POST['term_meta'] ) ) {
+        $t_id = $term_id;
+        $term_meta = get_option( "taxonomy_$t_id" );
+        $cat_keys = array_keys( $_POST['term_meta'] );
+        foreach ( $cat_keys as $key ) {
+            if ( isset ( $_POST['term_meta'][$key] ) ) {
+                $term_meta[$key] = $_POST['term_meta'][$key];
+            }
+        }
+       update_option( "taxonomy_$t_id", $term_meta );
+    }
+}
+
+// this action use for save field value of edit form of taxonomy 
+add_action( 'edited_product_cat', 'product_cat_cat_head_link_save', 10, 2 );  
+// this action use for save field value of add form of taxonomy 
+add_action( 'create_product_cat', 'product_cat_cat_head_link_save', 10, 2 );
+
 // hien thi ngoai font end
 global $post;
 
